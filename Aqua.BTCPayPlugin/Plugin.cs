c618213ff132e3ddb75a8bbd727a7344c25352ef
rockstardev <5191402+rockstardev@users.noreply.github.com>
2024-11-22 10:35:57 -0600

    Migrating plugin to 2.0

--- Aqua.BTCPayPlugin/Plugin.cs
+++ Aqua.BTCPayPlugin/Plugin.cs
@@ -1,4 +1,5 @@
 using Aqua.BTCPayPlugin.Services;
+using BTCPayServer;
 using BTCPayServer.Abstractions.Contracts;
 using BTCPayServer.Abstractions.Models;
 using BTCPayServer.Abstractions.Services;
@@ -10,13 +11,14 @@ namespace Aqua.BTCPayPlugin;
 public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
-    {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
-    };
+    [
+        new() {Identifier = nameof(BTCPayServer), Condition = ">=2.0.0"}
+    ];
 
     public override void Execute(IServiceCollection services)
     {
-        services.AddSingleton<IUIExtension>(new UIExtension("TemplatePluginHeaderNav", "header-nav"));
+        services.AddUIExtension("header-nav", "TemplatePluginHeaderNav");
+        
         services.AddHostedService<ApplicationPartsLogger>();
         services.AddSingleton<MyPluginService>();
     }
