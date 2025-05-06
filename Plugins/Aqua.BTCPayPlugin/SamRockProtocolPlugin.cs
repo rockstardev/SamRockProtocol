8309d0935fbffc9dc223d9cbc2adc01153f51f60
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-05-06 05:06:37 -0500

    Bump Aqua.BTCPayPlugin to 1.0.14, fixing dependency on BoltzExchanger

--- Plugins/Aqua.BTCPayPlugin/SamRockProtocolPlugin.cs
+++ Plugins/Aqua.BTCPayPlugin/SamRockProtocolPlugin.cs
@@ -12,7 +12,7 @@ public class SamRockProtocolPlugin : BaseBTCPayServerPlugin
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     [
         new() { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" },
-        new() { Identifier = nameof(BTCPayServer.RockstarDev.Plugins.BoltzExchanger), Condition = ">=0.0.1" }
+        new() { Identifier = "BTCPayServer.RockstarDev.Plugins.BoltzExchanger", Condition = ">=0.0.1" }
     ];
 
     public static bool IsDevMode =>
