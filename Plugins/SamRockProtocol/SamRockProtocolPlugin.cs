b2dd331203845e1e6cbf0858847ca7f70f155b1d
nicolas.dorier <nicolas.dorier@gmail.com>
2025-07-16 16:51:10 +0900

    Remove left over of workaround due to btcpayserver plugins dependency not working

--- Plugins/SamRockProtocol/SamRockProtocolPlugin.cs
+++ Plugins/SamRockProtocol/SamRockProtocolPlugin.cs
@@ -14,9 +14,8 @@ public class SamRockProtocolPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     [
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" },
-        // dependencies not working within BTCPay system
-        //new() { Identifier = "BTCPayServer.RockstarDev.Plugins.BoltzExchanger", Condition = ">=0.0.1" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.1.7" },
+        new() { Identifier = "BTCPayServer.Plugins.Boltz", Condition = ">=2.1.12" }
     ];
 
     public static bool IsDevMode =>
@@ -29,16 +28,6 @@ public class SamRockProtocolPlugin : BaseBTCPayServerPlugin
 
     public override void Execute(IServiceCollection services)
     {
-        var boltzPlugin = new BoltzPlugin();
-        boltzPlugin.Execute(services);
-        var partManager = services.BuildServiceProvider().GetRequiredService<ApplicationPartManager>();
-        var pluginAssembly = typeof(BoltzPlugin).Assembly;
-        foreach (ApplicationPart applicationPart in ApplicationPartFactory.GetApplicationPartFactory(pluginAssembly).GetApplicationParts(pluginAssembly))
-        {
-            partManager.ApplicationParts.Add(applicationPart);
-        }
-
-
         //services.AddUIExtension("store-wallets-nav", "AquaSidebarNav");
         services.AddUIExtension("dashboard-setup-guide-payment", "SamRockProtocolSetupPayments");
         services.AddUIExtension("store-integrations-nav", "SamRockProtocolNav");
